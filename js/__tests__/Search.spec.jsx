c70491c3348767fd20fe2765920524538017f91f
Carlo Santos <carlox7@gmail.com>
2017-07-13 15:16:35 -0700

    updating jest and npm

--- js/__tests__/Search.spec.jsx
+++ js/__tests__/Search.spec.jsx
@@ -5,7 +5,7 @@ import renderer from 'react-test-renderer';
 import Search from '../Search';
 
 test('Search renders correctly', () => {
-  const component = renderer.create(<search />);
+  const component = renderer.create(<Search />);
   const tree = component.toJSON();
   expect(tree).toMatchSnapshot();
 });
